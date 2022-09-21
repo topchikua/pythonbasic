4740a7d440027e82f84b4217ba3d76b93b8942a1
topchikua <52373889+topchikua@users.noreply.github.com>
2022-09-21 20:16:03 +0300

    Create todo2109.py

---
+++ 7 lesson/todo2109.py
@@ -0,0 +1,31 @@
+'''for value in 1, 2, 3, 4:
+    print(value)
+
+
+for value in 'hello', 'world', True, False, 1, 2.5:
+    print(value)'''
+
+'''for value in 'hello world':
+    print(value)
+
+for value in 'hello', 'world':
+    print(value)'''
+
+'''second_obj = 123
+
+for value in value:
+    print(value)'''
+
+'''print(range(1, 101))
+print(range(от, до, шаг))
+print(list(range(1, 101)))'''
+
+'''print(list(range(-111, 10, 5)))
+print(list(range(-20, 10, 3)))
+print(list(range(-20, 10, 3)))'''
+
+
+
+
+
+
