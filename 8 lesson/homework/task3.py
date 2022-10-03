91d1bad7e19c135b1761d9e640af242e8f9e6830
topchikua <52373889+topchikua@users.noreply.github.com>
2022-10-03 21:50:01 +0300

    Update task3.py

--- 8 lesson/homework/task3.py
+++ 8 lesson/homework/task3.py
@@ -1,44 +1,46 @@
 import string
 
 """
-Напишите программу где пользователь вводит пароль, а программа проверяет сложность пароля и выводит свой результат в оценочной шкале от 1 до 5.
-1 - у пользователя пароль == 'qwerty' or 'admin' или пароль пустой
-2 - у пользователя только цифры или спец. символы или все буквы в верхнем или нижнем регистре
-3- у пользователя в пароле соблюдается два условия из (цифры, буква нижнего регистра, буква верхнего регистра, спец. символы)
-4. у пользователя в пароле соблюдается три условия из (цифры, буква нижнего регистра, буква верхнего регистра, спец. символы)
-5 - у пользователя есть цифры, буквы нижнего и верхнего регистра, спец. символы и длинна пароля больше 8 символов
+Напишите программу где пользователь вводит пароль, а программа проверяет сложность пароля 
+и выводит свой результат в оценочной шкале от 1 до 5.
+ 1 - у пользователя пароль == 'qwerty' or 'admin' или пароль пустой
+ 2 - у пользователя только цифры или спец. символы или все буквы в верхнем или нижнем регистре
+ 3 - у пользователя в пароле соблюдается два условия из 
+ (цифры, буква нижнего регистра, буква верхнего регистра, спец. символы)
+ 4 - у пользователя в пароле соблюдается три условия из 
+ (цифры, буква нижнего регистра, буква верхнего регистра, спец. символы)
+ 5 - у пользователя есть цифры, буквы нижнего и верхнего регистра, спец. символы и длинна пароля больше 8 символов
 """
 
-pswrd = input('Введите пароль: ')
-symbol_count = len(pswrd) > 8
 pass_wrong = ['Qwerty123', 'admin', 'password', '123456789', 'qwerty', '']
-test = [False, False, False, False]
-pass_complexity = 0
-for symbol in pswrd:
+pass_detail = [False, False, False, False]
+password = input('Введите пароль: ')
+symbol_count = len(password) > 8
+for symbol in password:
     if symbol.isdigit():
-        test.pop(0)
-        test.insert(0, True)
+        pass_detail.pop(0)
+        pass_detail.insert(0, True)
     elif symbol.islower():
-        test.pop(1)
-        test.insert(1, True)
+        pass_detail.pop(1)
+        pass_detail.insert(1, True)
     elif symbol.isupper():
-        test.pop(2)
-        test.insert(2, True)
+        pass_detail.pop(2)
+        pass_detail.insert(2, True)
     elif string.punctuation.find(symbol):
-        test.pop(3)
-        test.insert(3, True)
+        pass_detail.pop(3)
+        pass_detail.insert(3, True)
     else:
         ...
-if pass_wrong.count(pswrd) > 0:
+if pass_wrong.count(password) > 0:
     pass_complexity = 1
-elif symbol_count and test.count(True) == 4:
+elif symbol_count and pass_detail.count(True) == 4:
     pass_complexity = 5
-elif test.count(True) == 3:
+elif pass_detail.count(True) == 3:
     pass_complexity = 4
-elif test.count(True) == 2:
+elif pass_detail.count(True) == 2:
     pass_complexity = 3
-elif test.count(True) == 1:
+elif pass_detail.count(True) == 1:
     pass_complexity = 2
 else:
-    ...
+    pass_complexity = 'Error'
 print(f'\nНадежность вашего пароля: {pass_complexity}')
