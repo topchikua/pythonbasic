ff61dfe4b799c1fd93655c24a1dd8c286e7824da
topchikua <52373889+topchikua@users.noreply.github.com>
2022-10-22 20:04:49 +0300

    Update task1.py

--- 13 lesson/homework/task1.py
+++ 13 lesson/homework/task1.py
@@ -5,14 +5,12 @@ from random import randint
 В исходном списке минимум 2 элемента.
 """
 
-
 def change(lst):
     first = lst[0]
     last = lst[-1]
     lst[-1] = first
     lst[0] = last
     return lst
-
 
 lst = [randint(-10, 10) for i in range(randint(2, 7))]
 
