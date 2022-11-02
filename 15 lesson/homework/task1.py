8e50efd054aaef389f79a3a71053fb0b26e19019
topchikua <52373889+topchikua@users.noreply.github.com>
2022-11-02 21:50:01 +0200

    Create task1.py

---
+++ 15 lesson/homework/task1.py
@@ -0,0 +1,51 @@
+import pprint
+
+
+"""
+Необходимо создать класс Human с атрибутами:
+name
+surname
+age
+phone
+address
+Атрибуты должны заполняться в методе __init__
+
+Так-же нужно написать методы:
+get_info() - который возвращает словарь в котором находится информация о человеке
+call(phone_number) - который будет выводить "{self.phone} вызывает абонента {phone_number}"
+Нужно создать 3 обьекта класса Human и вызвать у них метод get_info
+
+В качестве решения нужно отправить ссылку на GitHub репозиторий.
+"""
+
+
+class Human:
+
+    def __init__(self, name, surname, age, phone, address):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.phone = phone
+        self.address = address
+
+    def get_info(self):
+        return self.__dict__
+
+    def call(self, phone_number):
+        print(f'{self.phone} вызывает абонента {phone_number}')
+
+
+Anton = Human('Anton', 'Honcharuk', 30, '+380937778899', 'City gb')
+Max = Human('Max', 'Plaxoy', 25, '+380938978899', 'City sd')
+Vasyl = Human('Vasyl', 'Petrov', 41, '+380937778300', 'City ds')
+phone_number = 3700
+
+pprint.pprint(Anton.__dict__)
+Anton.call(phone_number)
+print('----------------------\n')
+pprint.pprint(Max.__dict__)
+Anton.call(phone_number)
+print('----------------------\n')
+pprint.pprint(Vasyl.__dict__)
+Anton.call(phone_number)
+print('----------------------')
